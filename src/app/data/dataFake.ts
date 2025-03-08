69fba99fb2e9b1f1e2508fd2f73ecde4e4cb51b0
juliaknmelo <ju-kar98@hotmail.com>
2025-03-08 19:18:09 -0300

    commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "WOMAKERS CODE - Mulheres na programação",
+    "description": "Desde 2015, conectando mulheres à tecnologia e abrindo caminhos do primeiro código à liderança, promovendo a representação feminina em todas as áreas da inovação por meio de ações intencionais e oportunidades reais.",
+    "photoCover":"../../assets/materia1.jpg",
+    "link": "https://womakerscode.org/?gad_source=1&gclid=Cj0KCQiA8q--BhDiARIsAP9tKI0rHwoO18IW_XzE5fITsaLnBKZd-Yseu9xtCCZFslEDheTkvBgRVw4aAjvEEALw_wcB"
+  },
+  {
+    "id":"2",
+    "title": "Generation Brasil - ponte entre a tecnologia e a empregabilidade",
+    "description": "Promovendo a mobilidade econômica através do emprego, preparando, apoiando e empregando as pessoas em carreiras transformadoras que, de outra forma, seriam inacessíveis.",
+    "photoCover":"../../assets/materia2.png",
+    "link": "https://brazil.generation.org/"
+  },
+  {
+    "id":"3",
+    "title": "{REPROGRAMA} - Mulheres na tecnologia ",
+    "description": "Oferecemos cursos de programação completamente gratuitos para mulheres e adolescentes em situações de vulnerabilidade social, econômica e de gênero, priorizando em nossos processos seletivos negras, trans e travestis.",
+    "photoCover":"../../assets/materia3.jpg",
+    "link": "https://reprograma.com.br/?gad_source=1&gclid=Cj0KCQiA8q--BhDiARIsAP9tKI0-GBBbbKtMZNBd3uiei04z9jxmweoHr1ue67EhQsAfiRv9uCDiYPQaAqMiEALw_wcB"
+  },
+  {
+    "id":"4",
+    "title": "D I O - cursos de tecnologia",
+    "description": "+1.750 cursos, bootcamps, projetos e formações nas carreiras de back-end, front-end, mobile, games, data, inteligência artificial & cloud para elevar o seu nível profissional e conquistar oportunidades nas empresas mais desejadas do mundo",
+    "photoCover":"../../assets/materia4.jpg",
+    "link": "https://www.dio.me/"
+  }
+]
