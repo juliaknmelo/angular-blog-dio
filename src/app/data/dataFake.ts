d4746dcfd8c2dce8101959668a1280b77ab170a4
juliaknmelo <ju-kar98@hotmail.com>
2025-03-25 22:39:42 -0300

    att

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,30 +1,37 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "WOMAKERS CODE - Mulheres na programação",
-    "description": "Desde 2015, conectando mulheres à tecnologia e abrindo caminhos do primeiro código à liderança, promovendo a representação feminina em todas as áreas da inovação por meio de ações intencionais e oportunidades reais.",
+    "title": "WoMakersCode",
+    "description": "A WoMakersCode é uma ONG fundada em 2015 pela Cynthia Zanoni, movida por seu sonho de tornar a tecnologia uma carreira acessível a todas as mulheres. Com o objetivo de fortalecer o protagonismo feminino na área de tecnologia, impulsiando o desenvolvimetno profissional e econômico para construir um futuro mais inclusivo e equitativo. A iniciativa conta com programas de bootcamp, cursos, uma comunidade participativa e oportunidades de voluntáriado. Para saber clique no link abaixo",
     "photoCover":"../../assets/materia1.jpg",
     "link": "https://womakerscode.org/?gad_source=1&gclid=Cj0KCQiA8q--BhDiARIsAP9tKI0rHwoO18IW_XzE5fITsaLnBKZd-Yseu9xtCCZFslEDheTkvBgRVw4aAjvEEALw_wcB"
   },
   {
     "id":"2",
-    "title": "Generation Brasil - ponte entre a tecnologia e a empregabilidade",
-    "description": "Promovendo a mobilidade econômica através do emprego, preparando, apoiando e empregando as pessoas em carreiras transformadoras que, de outra forma, seriam inacessíveis.",
+    "title": "Generation Brasil",
+    "description": "A Generation é uma rede global sem fins lucrativos que apoia as pessoas a alcançarem mobilidade económica e uma vida melhor. Promovendo a mobilidade econômica através do emprego, preparando, apoiando e empregando as pessoas em carreiras transformadoras que, de outra forma, seriam inacessíveis. A ONG conta com opções de bootcamps em várias da tecnologia, saiba mais no link abaixo.",
     "photoCover":"../../assets/materia2.png",
     "link": "https://brazil.generation.org/"
   },
   {
     "id":"3",
-    "title": "{REPROGRAMA} - Mulheres na tecnologia ",
-    "description": "Oferecemos cursos de programação completamente gratuitos para mulheres e adolescentes em situações de vulnerabilidade social, econômica e de gênero, priorizando em nossos processos seletivos negras, trans e travestis.",
+    "title": "{Reprograma}",
+    "description": "É uma iniciativa de impacto social que tem como missão diminuir a lacuna de gênero e raça no setor de tecnologia por meio da educação e trazer mais diversidade ao setor, capacitando mulheres em situações de vulnerabilidade, priorizando em seus processos seletivos negras e/ou trans e travestis. Seus principais projetos são cursos gratuitos na área de tecnologia, em back-end, front-end e análise de dados",
     "photoCover":"../../assets/materia3.jpg",
     "link": "https://reprograma.com.br/?gad_source=1&gclid=Cj0KCQiA8q--BhDiARIsAP9tKI0-GBBbbKtMZNBd3uiei04z9jxmweoHr1ue67EhQsAfiRv9uCDiYPQaAqMiEALw_wcB"
   },
   {
     "id":"4",
-    "title": "D I O - cursos de tecnologia",
-    "description": "+1.750 cursos, bootcamps, projetos e formações nas carreiras de back-end, front-end, mobile, games, data, inteligência artificial & cloud para elevar o seu nível profissional e conquistar oportunidades nas empresas mais desejadas do mundo",
+    "title": "D I O",
+    "description": "Plataforma com +1.750 cursos, bootcamps, projetos e formações nas carreiras de back-end, front-end, mobile, games, data, inteligência artificial & cloud para elevar o seu nível profissional e conquistar oportunidades nas empresas mais desejadas do mundo. Conta com bootcamps gratuitos em parceria com grandes empresas com chances de participação em processos seletivos.",
     "photoCover":"../../assets/materia4.jpg",
     "link": "https://www.dio.me/"
+  },
+  {
+    "id":"5",
+    "title": "Decola Tech",
+    "description": "Programa de bootcamp oferecido pela empresa Avanade em parceria com a plataforma de cursos DIO, conta com aulas na áreas de back-end, front-end e dados, ofertados de forma gratuita e que podem ser portas para participação de processos seletivos na empresa apoiadora.",
+    "photoCover":"../../assets/materia5.jpg",
+    "link": "https://www.dio.me/bootcamp/decola-tech-2025"
   }
 ]
